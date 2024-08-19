4d2ca02a0aae98ea4789d3a8694724e15313e551
leiyang2000 <syeipcr0909@gmail.com>
2024-08-18 21:21:44 -0500

    twosum

---
+++ 1.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+
+        for i in range(len(nums)):
+
+            if target - nums[i] in d:
+
+                return [i, d[target - nums[i]]]
+            
+            d[nums[i]] = i
+            
+s = Solution()
+
+ret = s.twoSum(nums = [1,2,3,4],target = 7)
+
+print(ret)
